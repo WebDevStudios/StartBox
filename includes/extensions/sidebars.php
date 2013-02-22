9d7676c207ee503bb0b33c6f4ce3925898c505f4
Brian Richards <brian@rzen.net>
2013-02-22 09:29:13 -0500

    OCD whitespace

--- includes/extensions/sidebars.php
+++ includes/extensions/sidebars.php
@@ -22,31 +22,32 @@ if (!current_theme_supports( 'sb-sidebars' )) return;
 function sb_sidebars_init() {
 	// Add custom post type
 	register_post_type( 'sidebar', array(
-		'labels' 				=> array(
-			'name' 				=> _x( 'Sidebars', 'post type general name', 'startbox' ),
-			'singular_name' 	=> _x( 'Sidebar', 'post type singular name', 'startbox' ),
-			'add_new' 			=> _x( 'Add New', 'post type add link', 'startbox' ),
-			'add_new_item' 		=> __( 'Add New Sidebar', 'startbox' ),
-			'edit_item' 		=> __( 'Edit Sidebar', 'startbox' ),
-			'new_item' 			=> __( 'New Sidebar', 'startbox' ),
-			'view_item' 		=> __( 'View Sidebar', 'startbox' ),
-			'search_items' 		=> __( 'Search Sidebars', 'startbox' ),
-			'not_found' 		=> __( 'No sidebars found', 'startbox' ),
-			'not_found_in_trash'=> __( 'No sidebars found in Trash', 'startbox' ),
-			'parent_item_colon' => '' ),
-		'label' 				=> __( 'Sidebars', 'startbox' ),
-		'singular_label' 		=> __( 'Sidebar', 'startbox' ),
-		'public' 				=> false,
-		'exclude_from_search' 	=> true,
-		'show_ui' 				=> true,
-		'hierarchical' 			=> false,
-		'rewrite' 				=> false,
-		'query_var' 			=> false,
-		'supports' 				=> array( 'title' ),
-		'show_in_menu'			=> 'themes.php',
-		'capability_type'		=> 'post',
-		'show_in_nav_menus' 	=> false,
-		'register_meta_box_cb' 	=> 'sb_sidebars_metabox_setup' ));
+		'labels' => array(
+			'name'               => _x( 'Sidebars', 'post type general name', 'startbox' ),
+			'singular_name'      => _x( 'Sidebar', 'post type singular name', 'startbox' ),
+			'add_new'            => _x( 'Add New', 'post type add link', 'startbox' ),
+			'add_new_item'       => __( 'Add New Sidebar', 'startbox' ),
+			'edit_item'          => __( 'Edit Sidebar', 'startbox' ),
+			'new_item'           => __( 'New Sidebar', 'startbox' ),
+			'view_item'          => __( 'View Sidebar', 'startbox' ),
+			'search_items'       => __( 'Search Sidebars', 'startbox' ),
+			'not_found'          => __( 'No sidebars found', 'startbox' ),
+			'not_found_in_trash' => __( 'No sidebars found in Trash', 'startbox' ),
+			'parent_item_colon'  => '' ),
+		'label'                => __( 'Sidebars', 'startbox' ),
+		'singular_label'       => __( 'Sidebar', 'startbox' ),
+		'public'               => false,
+		'exclude_from_search'  => true,
+		'show_ui'              => true,
+		'hierarchical'         => false,
+		'rewrite'              => false,
+		'query_var'            => false,
+		'supports'             => array( 'title' ),
+		'show_in_menu'         => 'themes.php',
+		'capability_type'      => 'post',
+		'show_in_nav_menus'    => false,
+		'register_meta_box_cb' => 'sb_sidebars_metabox_setup'
+	) );
 
 }
 add_action( 'init', 'sb_sidebars_init' );
@@ -96,7 +97,12 @@ add_action( 'load-post-new.php', 'sb_sidebars_includes' );
  */
 function sb_sidebars_bar_init() {
 	global $wp_admin_bar;
-    $wp_admin_bar->add_menu( array( 'id' => 'sb-sidebars', 'parent' => 'appearance', 'title' => __('Sidebars', 'startbox'), 'href' => admin_url( 'edit.php?post_type=sidebar' ) ) );
+	$wp_admin_bar->add_menu( array(
+		'id'     => 'sb-sidebars',
+		'parent' => 'appearance',
+		'title'  => __('Sidebars', 'startbox'),
+		'href'   => admin_url( 'edit.php?post_type=sidebar' )
+	) );
 }
 add_action( 'wp_before_admin_bar_render', 'sb_sidebars_bar_init' );
 
@@ -130,7 +136,7 @@ function sb_sidebars_metabox_setup() {
 	// Shortcode metabox
 	add_meta_box( "sidebar-shortcode", 'Shortcode', 'sb_sidebars_shortcode_metabox', 'sidebar', 'side', 'default' );
 
-	// Get all post types, but only if they appear in nav menus
+	// Get all post types that are set to show in nav menus
 	if ( $post_types = get_post_types( array( 'show_in_nav_menus' => true ), 'object' ) ) {
 		foreach ( $post_types as $post_type ) {
 			$post_type = apply_filters( 'sb_sidebars_meta_box_object', $post_type );
@@ -141,7 +147,7 @@ function sb_sidebars_metabox_setup() {
 		}
 	}
 
-	// Get all taxonomies, but only if they appear in nav menus
+	// Get all taxonomies that are set to show in nav menus
 	if ( $taxonomies = get_taxonomies( array( 'show_in_nav_menus' => true ), 'object' ) ) {
 		foreach ( $taxonomies as $tax ) {
 			$tax = apply_filters( 'sb_sidebars_meta_box_object', $tax );
@@ -225,11 +231,11 @@ function sb_sidebars_post_type_meta_box( $post, $post_type ) {
 
 	// Arguments for quering posts
 	$args = array(
-		'order' => 'ASC',
-		'orderby' => 'title',
-		'posts_per_page' => -1,
-		'post_type' => $post_type_name,
-		'suppress_filters' => true,
+		'order'                  => 'ASC',
+		'orderby'                => 'title',
+		'posts_per_page'         => -1,
+		'post_type'              => $post_type_name,
+		'suppress_filters'       => true,
 		'update_post_term_cache' => false,
 		'update_post_meta_cache' => false
 	);
@@ -285,15 +291,15 @@ function sb_sidebars_taxonomy_meta_box( $post, $taxonomy ) {
 	$taxonomy_name = $taxonomy['args']->name;
 
 	$args = array(
-		'child_of' => 0,
-		'exclude' => '',
-		'hide_empty' => false,
-		'hierarchical' => 1,
-		'include' => '',
+		'child_of'                 => 0,
+		'exclude'                  => '',
+		'hide_empty'               => false,
+		'hierarchical'             => 1,
+		'include'                  => '',
 		'include_last_update_time' => false,
-		'order' => 'ASC',
-		'orderby' => 'name',
-		'pad_counts' => false,
+		'order'                    => 'ASC',
+		'orderby'                  => 'name',
+		'pad_counts'               => false,
 	);
 
 	$terms = get_terms( $taxonomy_name, $args );
@@ -366,12 +372,12 @@ class SB_Sidebars_Checklist extends Walker_Nav_Menu  {
 function sb_sidebars_save( $post_id ) {
 	// Verify we should actually be saving any data
 	if (
-		!sb_verify_post_type( 'sidebar' ) || 				// If it's not a sidebar post type,
-		defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ||	// or if it's autosaving,
-		!isset($_POST['sidebar_replaced']) ||				// or we don't have some postdata,
-		!current_user_can('edit_theme_options')	||			// or the current user can't edit themes,
-		!isset( $_POST['_sb_sidebars_nonce'] ) ||			// or the NONCE is not set,
-		!wp_verify_nonce( $_POST['_sb_sidebars_nonce'], 'sb-sidebar-update' ) // or the NONCE is invalid
+		! sb_verify_post_type( 'sidebar' )               // If it's not a sidebar post type,
+		|| defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE // or if it's autosaving,
+		|| ! isset($_POST['sidebar_replaced'])           // or we don't have some postdata,
+		|| ! current_user_can('edit_theme_options')      // or the current user can't edit themes,
+		|| ! isset( $_POST['_sb_sidebars_nonce'] )       // or the NONCE is not set,
+		|| ! wp_verify_nonce( $_POST['_sb_sidebars_nonce'], 'sb-sidebar-update' ) // or the NONCE is invalid
 	)
 		return $post_id;									// skip the rest...
 
