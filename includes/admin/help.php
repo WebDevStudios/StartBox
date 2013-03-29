c56a6ff7eaf7307f84c9dc038a261fab2ed30a37
Brian Richards <brian@rzen.net>
2013-03-29 18:52:26 -0400

    Add link the SB support forum in the Need Help metabox, props @williamsba

--- includes/admin/help.php
+++ includes/admin/help.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Registers a settings metabox to direct users to the hepl tab
+ * Registers a settings metabox to direct users to the help tab
  */
 class sb_settings_help extends sb_settings {
 
@@ -14,6 +14,7 @@ class sb_settings_help extends sb_settings {
 
 	function admin_form() {
 		echo '<p>' . __( 'Struggling with some of the theme options or settings? Click on the "Help" tab above.', 'startbox' ) . '</p>';
+		echo '<p>' . sprintf( __( 'You can also visit the StartBox <a href="%s" target="_blank">support forum</a>', 'startbox' ), 'http://wpstartbox.com/support/' ) . '</p>';
 	}
 
 }
