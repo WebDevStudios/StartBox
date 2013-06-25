3d9c62ec07b4def6f13874d30f630777b2599fc3
Brian Richards <brian@rzen.net>
2013-06-25 10:57:28 -0400

    Create SB_Breadcrumbs placeholder

---
+++ startbox/classes/SB_Breadcrumbs.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * StartBox Breadcrumbs
+ *
+ * Base class for hanling breadcrumb page navigation
+ *
+ * @package StartBox
+ * @subpackage Classes
+ * @since 3.0.0
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
