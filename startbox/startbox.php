60234aff345a6709bb129f7c8729376c6c9f93e4
Brian Richards <brian@rzen.net>
2013-06-24 23:15:12 -0400

    Change constants to NOT include a trailing slash

--- startbox/startbox.php
+++ startbox/startbox.php
@@ -40,20 +40,21 @@ if ( ! class_exists('StartBox') ) {
 
 			define( 'SB_VERSION',      '3.0.0' );
 
-			define( 'THEME_DIR',       trailingslashit( get_template_directory() ) );
-			define( 'THEME_URI',       trailingslashit( get_template_directory_uri() ) );
-			define( 'CHILD_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
-			define( 'CHILD_THEME_URI', trailingslashit( get_stylesheet_directory_uri() ) );
-			define( 'SB_DIR',          trailingslashit( trailingslashit( THEME_DIR ) . basename( dirname( __FILE__ ) ) ) );
-			define( 'SB_URI',          trailingslashit( trailingslashit( THEME_URI ) . basename( dirname( __FILE__ ) ) ) );
-			define( 'SB_ADMIN',        trailingslashit( SB_DIR . 'admin' ) );
-			define( 'SB_CLASSES',      trailingslashit( SB_DIR . 'classes' ) );
-			define( 'SB_CSS',          trailingslashit( SB_URI . 'css' ) );
-			define( 'SB_EXTENSIONS',   trailingslashit( SB_DIR . 'extensions' ) );
-			define( 'SB_IMAGES',       trailingslashit( SB_DIR . 'images' ) );
-			define( 'SB_JS',           trailingslashit( SB_URI . 'js' ) );
-			define( 'SB_LANGUAGES',    trailingslashit( SB_DIR . 'languages' ) );
-			define( 'SB_WIDGETS',      trailingslashit( SB_DIR . 'widgets' ) );
+			// Define all our paths
+			define( 'THEME_DIR',       get_template_directory() );
+			define( 'THEME_URI',       get_template_directory_uri() );
+			define( 'CHILD_THEME_DIR', get_stylesheet_directory() );
+			define( 'CHILD_THEME_URI', get_stylesheet_directory_uri() );
+			define( 'SB_DIR',          trailingslashit( THEME_DIR ) . basename( dirname( __FILE__ ) ) );
+			define( 'SB_URI',          trailingslashit( THEME_URI ) . basename( dirname( __FILE__ ) ) );
+			define( 'SB_ADMIN',        trailingslashit( SB_DIR ) . 'admin' );
+			define( 'SB_CLASSES',      trailingslashit( SB_DIR ) . 'classes' );
+			define( 'SB_CSS',          trailingslashit( SB_URI ) . 'css' );
+			define( 'SB_EXTENSIONS',   trailingslashit( SB_DIR ) . 'extensions' );
+			define( 'SB_IMAGES',       trailingslashit( SB_DIR ) . 'images' );
+			define( 'SB_JS',           trailingslashit( SB_URI ) . 'js' );
+			define( 'SB_LANGUAGES',    trailingslashit( SB_DIR ) . 'languages' );
+			define( 'SB_WIDGETS',      trailingslashit( SB_DIR ) . 'widgets' );
 
 		}
 
@@ -63,10 +64,10 @@ if ( ! class_exists('StartBox') ) {
 		 * @since 3.0.0
 		 */
 		public function core() {
-			require_once( SB_EXTENSIONS . 'conditionals.php' );
-			require_once( SB_EXTENSIONS . 'custom.php' );
-			require_once( SB_EXTENSIONS . 'hooks.php' );
-			require_once( SB_EXTENSIONS . 'images.php' );
+			require_once( SB_EXTENSIONS . '/conditionals.php' );
+			require_once( SB_EXTENSIONS . '/custom.php' );
+			require_once( SB_EXTENSIONS . '/hooks.php' );
+			require_once( SB_EXTENSIONS . '/images.php' );
 		}
 
 		/**
@@ -75,19 +76,19 @@ if ( ! class_exists('StartBox') ) {
 		 * @since 3.0.0
 		 */
 		public function extensions() {
-			require_if_theme_supports( 'sb-breadcrumbs',  SB_CLASSES . 'SB_Breadcrumbs.php' );
-			require_if_theme_supports( 'sb-customizer',   SB_CLASSES . 'SB_Customizer.php' );
-			require_if_theme_supports( 'sb-layouts',      SB_CLASSES . 'SB_Layouts.php' );
-			require_if_theme_supports( 'sb-shortcodes',   SB_EXTENSIONS . 'shortcodes.php' );
-			require_if_theme_supports( 'sb-sidebars',     SB_CLASSES . 'SB_Sidebars.php' );
-			require_if_theme_supports( 'sb-updates',      SB_CLASSES . 'SB_Updater.php' );
+			require_if_theme_supports( 'sb-breadcrumbs',  SB_CLASSES . '/SB_Breadcrumbs.php' );
+			require_if_theme_supports( 'sb-customizer',   SB_CLASSES . '/SB_Customizer.php' );
+			require_if_theme_supports( 'sb-layouts',      SB_CLASSES . '/SB_Layouts.php' );
+			require_if_theme_supports( 'sb-sidebars',     SB_CLASSES . '/SB_Sidebars.php' );
+			require_if_theme_supports( 'sb-updates',      SB_CLASSES . '/SB_Updater.php' );
+			require_if_theme_supports( 'sb-shortcodes',   SB_EXTENSIONS . '/shortcodes.php' );
 
 			// Include all customization panels
-			foreach ( glob( SB_ADMIN . '*.php') as $sb_admin )
+			foreach ( glob( SB_ADMIN . '/*.php') as $sb_admin )
 				require_if_theme_supports( 'sb-customizer', $sb_admin );
 
 			// Include all packaged widgets
-			foreach ( glob( SB_WIDGETS . '*.php') as $sb_widget )
+			foreach ( glob( SB_WIDGETS . '/*.php') as $sb_widget )
 				require_if_theme_supports( 'sb-widgets', $sb_widget );
 
 		}
@@ -113,19 +114,19 @@ if ( ! class_exists('StartBox') ) {
 		public function scripts_and_styles() {
 
 			// Register Default Scripts
-			wp_register_script( 'colorbox',     SB_JS . 'jquery.colorbox.min.js', array( 'jquery' ), SB_VERSION );
-			wp_register_script( 'smoothScroll', SB_JS . 'jquery.smooth-scroll.min.js', array( 'jquery' ), SB_VERSION );
-			wp_register_script( 'startbox',     SB_JS . 'startbox.js', array( 'jquery' ), SB_VERSION );
-			wp_register_script( 'widgets',      SB_JS . 'widgets.js', array( 'jquery' ), SB_VERSION );
+			wp_register_script( 'colorbox',     SB_JS . '/jquery.colorbox.min.js', array( 'jquery' ), SB_VERSION );
+			wp_register_script( 'smoothScroll', SB_JS . '/jquery.smooth-scroll.min.js', array( 'jquery' ), SB_VERSION );
+			wp_register_script( 'startbox',     SB_JS . '/startbox.js', array( 'jquery' ), SB_VERSION );
+			wp_register_script( 'widgets',      SB_JS . '/widgets.js', array( 'jquery' ), SB_VERSION );
 
 			// Register Default Styles
-			wp_register_style( 'colorbox',      SB_CSS . 'colorbox.css', null, SB_VERSION, 'screen' );
-			wp_register_style( 'images',        SB_CSS . 'images.css', null, SB_VERSION );
-			wp_register_style( 'layouts',       SB_CSS . 'layouts.css', null, SB_VERSION );
-			wp_register_style( 'print',         SB_CSS . 'print.css', null, SB_VERSION, 'print' );
-			wp_register_style( 'reset',         SB_CSS . 'reset.css', null, SB_VERSION );
-			wp_register_style( 'shortcodes',    SB_CSS . 'shortcodes.css', null, SB_VERSION );
-			wp_register_style( 'typography',    SB_CSS . 'typography.css', null, SB_VERSION );
+			wp_register_style( 'colorbox',      SB_CSS . '/colorbox.css', null, SB_VERSION, 'screen' );
+			wp_register_style( 'images',        SB_CSS . '/images.css', null, SB_VERSION );
+			wp_register_style( 'layouts',       SB_CSS . '/layouts.css', null, SB_VERSION );
+			wp_register_style( 'print',         SB_CSS . '/print.css', null, SB_VERSION, 'print' );
+			wp_register_style( 'reset',         SB_CSS . '/reset.css', null, SB_VERSION );
+			wp_register_style( 'shortcodes',    SB_CSS . '/shortcodes.css', null, SB_VERSION );
+			wp_register_style( 'typography',    SB_CSS . '/typography.css', null, SB_VERSION );
 		}
 
 		// Setup the environment and register support for various WP features.
