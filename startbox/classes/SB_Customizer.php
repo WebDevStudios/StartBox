3bcea10a310ceb7b82ac43fc6d4b7a2072ee9a3a
Brian Richards <brian@rzen.net>
2013-07-25 22:11:54 -0400

    Register new SB_Customizer and SB_Sanitization classes
    Props @alexmansfield for his excellent work with Theme Foundation and
    Theme Toolkit

--- startbox/classes/SB_Customizer.php
+++ startbox/classes/SB_Customizer.php
@@ -3,257 +3,337 @@
  * StartBox Theme Customizer Settings
  *
  * Base extensions for the theme customizer introduced in WP3.4
+ * Many props to Alex Mansfield (@alexmansfield) for his work
+ * with Theme Foundation and Theme Toolkit.
+ *
+ * @link http://themefoundation.com/wordpress-theme-customizer/
  *
  * @package StartBox
- * @subpackage Classes
+ * @subpackage Customizer
  * @since 3.0.0
  * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
 // Check to see if current theme supports the customizer, skip the rest if not
-if (!current_theme_supports( 'sb-theme-customizer' )) return;
+if ( ! current_theme_supports( 'sb-customizer' ) ) return;
 
-$header_defaults = array(
-	'default-image'          => '',
-	'random-default'         => false,
-	'width'				      => 960,
-	'height'                 => 240,
-	'flex-height'            => false,
-	'flex-width'             => false,
-	'default-text-color'     => '#111111',
-	'header-text'            => true,
-	'uploads'                => true,
-	'wp-head-callback'       => '',
-	'admin-head-callback'    => '',
-	'admin-preview-callback' => '',
-);
-add_theme_support( 'custom-header', $header_defaults );
+/**
+ * Base class for supplimenting theme customizer
+ *
+ * @subpackage Classes
+ * @since 3.0.0
+ */
+class SB_Customizer {
 
+	/**
+	 * Instantiation method
+	 *
+	 * @since 3.0.0
+	 */
+	function __construct() {
 
-$background_defaults = array(
-	'default-color'          => '#ffffff',
-	'default-image'          => '',
-	'wp-head-callback'       => '_custom_background_cb',
-	'admin-head-callback'    => '',
-	'admin-preview-callback' => ''
-);
-add_theme_support( 'custom-background', $background_defaults );
+		// Register a customizer menu under Appearance
+		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
 
-add_action( 'customize_register', 'sb_customize_register' );
-function sb_customize_register($wp_customize) {
+		// Render all of our customizer settings and sections
+		add_action( 'customize_register', array( $this, 'customize_register' ) );
 
-	/********************** Remove Stock Sections **********************/
-	// http://wptheming.com/2012/06/add-options-to-theme-customizer-default-sections/
-	$wp_customize->remove_section( 'title_tagline' );
-	// $wp_customize->remove_section( 'colors' );
-	// $wp_customize->remove_section( 'header_image' );
-	// $wp_customize->remove_section( 'background_image' );
+	}
 
-	/********************** Register New Controls **********************/
+	/**
+	 * Register an admin menu item for the theme customizer
+	 *
+	 * @since 3.0.0
+	 */
+	function admin_menu() {
+	    add_theme_page( __( 'Customize', 'startbox' ), __( 'Customize', 'startbox' ), 'edit_theme_options', 'customize.php' );
+	}
 
-	class _Customize_Textarea_Control extends WP_Customize_Control {
+	/**
+	 * Displays customizer options
+	 *
+	 * Creates customizer options based on the customizer array,
+	 * usually set in the theme's functions.php file.
+	 *
+	 * @since 3.0.0
+	 * @param object $wp_customize The WordPress theme customizer object.
+	 */
+	public function customize_register( $wp_customize = null ) {
+
+		// Grab the currently registered sections
+		$sections = $wp_customize->sections();
+
+		// Pull back our registered settings
+		$customizer_settings = apply_filters( 'sb_customizer_settings', array() );
+
+		// If we have any settings, loop through and register each
+		if ( is_array( $customizer_settings ) && ! empty( $customizer_settings ) ) {
+			foreach ( $customizer_settings as $section_id => $section ) {
+
+				// Add the customizer section, if it doesn't exist already
+				if ( ! isset( $sections[$section_id] ) )
+					$this->add_section( $wp_customize, $section_id, $section );
+
+				// Add the section settings
+				foreach ( $section['settings'] as $setting ) {
+					$this->add_setting( $wp_customize, $section_id, $setting );
+					$this->add_control( $wp_customize, $section_id, $setting );
+				}
+
+			}
+		}
+
+	}
+
+	/**
+	 * Add a customizer section to the theme customizer
+	 *
+	 * @since 3.0.0
+	 * @param object $wp_customize The WordPress theme customizer object.
+	 * @param string $section_id   The unique name for this section
+	 * @param array  $section      Section specific data used to add the section to the customizer.
+	 */
+	public function add_section( $wp_customize = null, $section_id = '', $section = array() ) {
+
+		// Setup our section details with basic sanitization
+		$section_details                   = array();
+		$section_details['title']          = sanitize_text_field( $section['title'] );
+		$section_details['description']    = ! empty( $section['description'] ) ? esc_html( $section['description'] ) : '';
+		$section_details['priority']       = ! empty( $section['priority'] ) ? absint( $section['priority'] ) : 10;
+		$section_details['capability']     = ! empty( $section['capability'] ) ? sanitize_text_field( $section['capability'] ) : 'edit_theme_options';
+		$section_details['theme_supports'] = ! empty( $section['theme_supports'] ) ? sanitize_text_field( $section['theme_supports'] ) : null;
+
+		// Adds settings section to theme customizer.
+		$wp_customize->add_section( $section_id, $section_details );
+
+	}
+
+	/**
+	 * Adds individual settings and controls to the theme customizer
+	 *
+	 * @since 3.0.0
+	 * @param object $wp_customize    The WordPress theme customizer object
+	 * @param string $section_id      The section in which this setting belongs
+	 * @param array  $setting_details The setting details
+	 */
+	public function add_setting( $wp_customize = null, $section_id = '', $setting_details = array() ) {
+
+		// Setup our setting details with basic sanitization
+		$setting                         = array();
+		$setting['default']              = ! empty( $setting_details['default'] ) ? $setting_details['default'] : null;
+		$setting['capability']           = ! empty( $setting_details['capability'] ) ? $setting_details['capability'] : 'edit_theme_options';
+		$setting['theme_supports']       = ! empty( $setting_details['theme_supports'] ) ? $setting_details['theme_supports'] : null;
+		$setting['transport']            = ! empty( $setting_details['transport'] ) ? $setting_details['transport'] : 'refresh';
+		$setting['sanitize_js_callback'] = ! empty( $setting_details['sanitize_js_callback'] ) ? $setting_details['sanitize_js_callback'] : null;
+
+		// Setup the setting sanitization callback
+		if ( ! empty( $setting_details['sanitize_callback'] ) ) {
+			$setting['sanitize_callback'] = $setting_details['sanitize_callback'];
+		} else {
+
+			// If we don't specify what kind of data
+			// is valid, use the setting's type
+			if ( empty( $setting_details['valid'] ) )
+				$setting_details['valid'] = $setting_details['type'];
+
+			switch ( $setting_details['valid'] ) {
+				case 'text':
+					$setting['sanitize_callback'] = 'sanitize_text_field';
+					break;
+				case 'url':
+					$setting['sanitize_callback'] = 'esc_url_raw';
+					break;
+				case 'email':
+					$setting['sanitize_callback'] = 'sanitize_email';
+					break;
+				case 'radio':
+				case 'select':
+					$setting['sanitize_callback'] = array( new SB_Sanitization( $setting_details['choices'] ), 'sanitize_multiple_choice' );
+					break;
+				case 'checkbox':
+					$setting['sanitize_callback'] = array( new SB_Sanitization(), 'sanitize_html' );
+					break;
+				case 'html':
+					$setting['sanitize_callback'] = array( new SB_Sanitization(), 'sanitize_html' );
+					break;
+				case 'integer':
+					$setting['sanitize_callback'] = array( new SB_Sanitization(), 'sanitize_integer' );
+					break;
+				case 'color':
+					$setting['sanitize_callback'] = 'sanitize_hex_color';
+					break;
+				default:
+					$setting['sanitize_callback'] = 'sanitize_text_field';
+					break;
+			}
+		}
+
+		// Adds setting to theme customizer.
+		$wp_customize->add_setting( $setting_details['id'], $setting );
+
+	}
+
+	/**
+	 * Add a controller for a given setting
+	 *
+	 * @since 3.0.0
+	 * @param object $wp_customize    The WordPress theme customizer object
+	 * @param string $section_id      The section in which this control belongs
+	 * @param array  $setting_details The setting details
+	 */
+	public function add_control( $wp_customize = null, $section_id = '', $setting_details = array() ) {
+
+		// Defines array to pass to add_control().
+		$control = array();
+		$control['section']  = sanitize_text_field( $section_id );
+		$control['label']    = isset( $setting_details['label'] ) ? sanitize_text_field( $setting_details['label'] ) : '';
+		$control['type']     = isset( $setting_details['type'] ) ? sanitize_text_field( $setting_details['type'] ) : 'text';
+		$control['priority'] = isset( $setting_details['priority'] ) ? absint( $setting_details['priority'] ) : 10;
+
+		// Set setting choices if they exist and this is a multiple choice setting
+		if (
+			isset( $setting_details['choices'] )
+			&& is_array( $setting_details['choices'] )
+			&& in_array( $setting_details['type'], array( 'select', 'radio' ) )
+		) {
+			$control['choices'] = $setting_details['choices'];
+		}
+
+		// Register the setting control
+		if ( in_array( $control['type'], array( 'text', 'checkbox', 'radio', 'select', 'dropdown-pages' ) ) ) {
+			$wp_customize->add_control( $setting_details['id'], $control );
+		} else {
+			switch ( $setting_details['type'] ) {
+				case 'color':
+					$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, $setting_details['id'], $control ) );
+					break;
+				case 'upload':
+					$wp_customize->add_control( new WP_Customize_Upload_Control( $wp_customize, $setting_details['id'], $control ) );
+					break;
+				case 'image':
+					$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, $setting_details['id'], $control ) );
+					break;
+				case 'textarea':
+					$wp_customize->add_control( new SB_Customize_Textarea_Control( $wp_customize, $setting_details['id'], $control ) );
+					break;
+			}
+		}
+	}
+}
+$GLOBALS['startbox']->customizer = new SB_Customizer;
+
+// Make sure WP_Customize_Control is available
+if ( class_exists( 'WP_Customize_Control' ) ) {
+
+	/**
+	 * Register textarea controller for the theme customizer
+	 *
+	 * @subpackage Classes
+	 * @link http://ottopress.com/2012/making-a-custom-control-for-the-theme-customizer
+	 * @since 3.0.0
+	 */
+	class SB_Customize_Textarea_Control extends WP_Customize_Control {
+
+		/**
+		 * @access public
+		 * @since 3.0.0
+		 * @var string The type of form element being generated.
+		 */
 		public $type = 'textarea';
 
+		/**
+		 * Overrides the render_content() function in the parent class
+		 *
+		 * @since 3.0.0
+		 */
 		public function render_content() {
 			?>
-			<label>
-			<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
-			<textarea rows="5" style="width:100%;" <?php $this->link(); ?>><?php echo esc_textarea( $this->value() ); ?></textarea>
-			</label>
+				<label>
+					<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+					<textarea rows="5" style="width:100%;" <?php $this->link(); ?>><?php echo esc_textarea( $this->value() ); ?></textarea>
+				</label>
 			<?php
 		}
 	}
-
-
-	/********************** Branding Section **********************/
-
-	$wp_customize->add_section( 'sb_branding', array(
-		'title'          => __( 'Title, Tagline & Branding', 'startbox' ),
-		'priority'       => 20,
-	) );
-
-	$wp_customize->add_setting( 'blogname', array(
-		'default'    => get_option( 'blogname' ),
-		'type'       => 'option',
-		'capability' => 'manage_options',
-	) );
-
-	$wp_customize->add_control( 'blogname', array(
-		'label'      => __( 'Site Title' ),
-		'section'    => 'sb_branding',
-		'priority'	=> 10
-	) );
-
-	$wp_customize->add_setting( 'blogdescription', array(
-		'default'    => get_option( 'blogdescription' ),
-		'type'       => 'option',
-		'capability' => 'manage_options',
-	) );
-
-	$wp_customize->add_control( 'blogdescription', array(
-		'label'      => __( 'Site Tagline' ),
-		'section'    => 'sb_branding',
-		'priority'	=> 20
-	) );
-
-	$wp_customize->add_setting( 'startbox[tagline]', array(
-		'type'       => 'option',
-		'capability' => 'manage_options',
-	) );
-
-	$wp_customize->add_control( 'startbox[tagline]', array(
-		'label'      => __( 'Display Site Tagline in Header' ),
-		'section'    => 'sb_branding',
-		'type'		 => 'checkbox',
-		'priority'	=> 30
-	) );
-
-	$wp_customize->add_setting( 'startbox[logo-type]', array(
-		'default'        => '',
-		'type'           => 'option',
-		'capability'     => 'edit_theme_options',
-	) );
-
-	$wp_customize->add_control( 'startbox[logo-type]', array(
-		'label'      => __( 'Logo Display' ),
-		'section'    => 'sb_branding',
-		'type'		 => 'select',
-		'choices'    => array(
-			'image' 	=> 'Image',
-			'text'		=> 'Site Title (plain text)',
-			'disabled'	=> 'Disabled'
-		),
-		'priority'	=> 40
-	) );
-
-	$wp_customize->add_setting( 'startbox[logo-align]', array(
-		'default'        => '',
-		'type'           => 'option',
-		'capability'     => 'edit_theme_options',
-	) );
-
-	$wp_customize->add_control( 'startbox[logo-align]', array(
-		'label'      => __( 'Logo Alignment' ),
-		'section'    => 'sb_branding',
-		'type'		 => 'select',
-		'choices'    => array(
-			'left' 		=> 'Left',
-			'center'	=> 'Center',
-			'right'		=> 'Right'
-		),
-		'priority'	=> 50
-	) );
-
-	$wp_customize->add_setting( 'startbox[logo-image]', array(
-		'default'        => '',
-		'type'           => 'option',
-		'capability'     => 'edit_theme_options',
-	) );
-
-	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'startbox[logo-image]', array(
-		'label'		=> __( 'Logo Image' ),
-		'section'	=> 'sb_branding',
-		'settings'	=> 'startbox[logo-image]',
-		'priority'	=> 60
-	)));
-
-	$wp_customize->add_setting( 'startbox[favicon]', array(
-		'default'        => '',
-		'type'           => 'option',
-		'capability'     => 'edit_theme_options',
-	) );
-
-	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'startbox[favicon]', array(
-		'label'		=> 'Favicon',
-		'section'	=> 'sb_branding',
-		'settings'	=> 'startbox[favicon]',
-		'priority'	=> 70
-	)));
-
-	/********************** Colors & Styles Section **********************/
-
-	$wp_customize->add_section( 'colors', array(
-		'title'          => __( 'Colors & Styles', 'startbox' ),
-		'priority'       => 30,
-	) );
-
-	$wp_customize->add_setting( 'background_color', array(
-		'default'        => get_theme_support( 'custom-background', 'default-color' ),
-		'theme_supports' => 'custom-background',
-
-		'sanitize_callback'    => 'sanitize_hex_color_no_hash',
-		'sanitize_js_callback' => 'maybe_hash_hex_color',
-	) );
-
-	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'background_color', array(
-		'label'   => __( 'Page Background Color' ),
-		'section' => 'colors',
-		'priority' => 1
-	) ) );
-
-	$wp_customize->add_setting( 'header_bgcolor', array(
-		'theme_supports' => array( 'custom-header', 'header-text' ),
-		'default'        => get_theme_support( 'custom-header', 'default-text-color' ),
-		'sanitize_callback'    => 'sanitize_hex_color_no_hash',
-		'sanitize_js_callback' => 'maybe_hash_hex_color',
-	) );
-
-	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'header_bgcolor', array(
-		'label'   => __( 'Header Background Color' ),
-		'section' => 'colors',
-		'priority' => 5
-	) ) );
-
-	/********************** Footer Section **********************/
-
-	$wp_customize->add_section( 'sb_footer', array(
-		'title'          => __( 'Footer', 'startbox' ),
-		'priority'       => 100,
-	) );
-
-	$wp_customize->add_setting( 'startbox[enable_rtt]', array(
-		'default'		=> true,
-		'type'			=> 'option',
-		'capability'	=> 'edit_theme_options',
-	) );
-
-	$wp_customize->add_control( 'startbox[enable_rtt]', array(
-		'label'			=> __( 'Enable "Return to Top" Link' ),
-		'section'		=> 'sb_footer',
-		'type'			=> 'checkbox',
-		'priority'		=> 10
-	) );
-
-	$wp_customize->add_setting( 'startbox[enable_admin]', array(
-		'default'		=> true,
-		'type'			=> 'option',
-		'capability'	=> 'edit_theme_options',
-	) );
-
-	$wp_customize->add_control( 'startbox[enable_admin]', array(
-		'label'			=> __( 'Enable Admin Links' ),
-		'section'		=> 'sb_footer',
-		'type'			=> 'checkbox',
-		'priority'		=> 20
-	) );
-
-	$wp_customize->add_setting( 'startbox[footer_text]', array(
-		'default'		=> 'Copyright &copy;[copyright year="' . date('Y') . '"] [site_link]. All Rights Reserved.
-		Proudly powered by [WordPress] and [StartBox].',
-		'type'			=> 'option',
-		'capability'    => 'edit_theme_options',
-	) );
-
-	$wp_customize->add_control( new _Customize_Textarea_Control( $wp_customize, 'startbox[footer_text]', array(
-		'label' 		=> __( 'Footer Text' ),
-		'section' 		=> 'sb_footer',
-		'settings'		=> 'startbox[footer_text]',
-		'priority'		=> 30
-	) ) );
-
 }
 
-add_action( 'customize_register', 'sb_customizer_controls', 0 );
-function sb_customizer_controls() {
+/**
+ * Base class for handling input sanitization
+ *
+ * @subpackage Classes
+ * @since 3.0.0
+ */
+class SB_Sanitization {
+
+	/**
+	 * Allowed inputs
+	 *
+	 * @since 3.0.0
+	 * @var array
+	 */
+	private $valid;
+
+	/**
+	 * Instantiation method, sets up valid inputs
+	 *
+	 * @since 3.0.0
+	 * @param array $valid An array of valid inputs
+	 */
+	function __construct( $valid = array() ) {
+		$this->valid = is_array( $valid ) ? $valid : array();
+	}
+
+	/**
+	 * Sanitizes multiple choice inputs
+	 *
+	 * Sanitizes select and radio inputs based on the valid
+	 * "options" array set in the original options array
+	 *
+	 * @since  3.0.0
+	 * @param  string $input Input data to be sanitized
+	 * @param  array  $valid Array of allowed values.
+	 * @return string        Valid option from $valid array matching $input, otherwise null.
+	 */
+	public function sanitize_multiple_choice( $input = '' ) {
+
+		if ( array_key_exists( $input, $this->valid ) )
+			return $input;
+		else
+			return null;
+
+	}
+
+	/**
+	 * Sanitizes checkbox (boolean) inputs
+	 *
+	 * @since  3.0.0
+	 * @param  string $input Input data to be sanitized
+	 * @return string        Returns the $valid string if equal to $input, otherwise null.
+	 */
+	public function sanitize_checkbox( $input = '' ) {
+		return ( ! empty( $input ) ) ? 'true' : 'false';
+	}
+
+	/**
+	 * Sanitizes HTML input
+	 *
+	 * @since  3.0.0
+	 * @param  string $input Input data to be sanitized
+	 * @return string        Sanitized HTML
+	 */
+	public function sanitize_html( $input = '' ) {
+		return wp_kses_post( force_balance_tags( $input ) );
+	}
+
+	/**
+	 * Sanitizes integer input
+	 *
+	 * @since  3.0.0
+	 * @param  string $input Input data to be sanitized
+	 * @return integer       Returns the $valid string after sanitization.
+	 */
+	public function sanitize_integer( $input = '' ) {
+		return is_numeric( $input ) ? intval( $input ) : absint( $input );
+	}
 
 }
