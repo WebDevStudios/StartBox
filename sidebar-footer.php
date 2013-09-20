3bbfbcb9907bb4c438ddcfdeccd8ed6a49bcd319
Greg Rickaby <gregrickaby@gmail.com>
2013-09-20 15:18:28 -0500

    Hooked up footer widgets

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -6,16 +6,16 @@
  */
 ?>
 	<div class="footer-widgets-1 widget-area col span_4">
-		<?php if ( ! dynamic_sidebar( 'sidebar-footer-1' ) ) : ?>
-		<?php endif; // end sidebar widget area ?>
+		<?php if ( ! dynamic_sidebar( 'footer_widget_area_1' ) ) { ?>
+		<?php } ?>
 	</div>
 
 	<div class="footer-widgets-2 widget-area col span_4">
-		<?php if ( ! dynamic_sidebar( 'sidebar-footer-2' ) ) : ?>
-		<?php endif; // end sidebar widget area ?>
+		<?php if ( ! dynamic_sidebar( 'footer_widget_area_2' ) ) { ?>
+		<?php } ?>
 	</div>
 
 	<div class="footer-widgets-3 widget-area col span_4">
-		<?php if ( ! dynamic_sidebar( 'sidebar-footer-3' ) ) : ?>
-		<?php endif; // end sidebar widget area ?>
+		<?php if ( ! dynamic_sidebar( 'footer_widget_area_2' ) ) { ?>
+		<?php } ?>
 	</div>
