c05dadb55b85554b4b7f40364ceea88c9b2be434
Greg Rickaby <gregrickaby@gmail.com>
2013-09-27 15:07:52 -0500

    Moved SASS folder out of /SBX

--- sass/config.rb
+++ sass/config.rb
@@ -1,10 +1,10 @@
 # Require any additional compass plugins here.
 
-http_path = "../../"
-css_dir = "../../"
-sass_dir = "/"
-images_dir = "../images"
-javascripts_dir = "../js"
+http_path = "/"
+css_dir = "/"
+sass_dir = "/sass"
+images_dir = "/images"
+javascripts_dir = "/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
