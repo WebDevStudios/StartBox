05acbf56a37afcfc106ec7c77eb8e3681d892f1e
Brad Williams <brad@webdevstudios.com>
2013-10-27 12:00:22 -0400

    Add resource links to the Help metabox

--- sbx/admin/help.php
+++ sbx/admin/help.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Registers a settings metabox to direct users to the help tab
+ * Registers a settings metabox to direct users to the help tab and resources
  */
 class sb_settings_help extends sb_settings {
 
@@ -15,8 +15,9 @@ class sb_settings_help extends sb_settings {
 	function admin_form() {
 		echo '<p>' . __( 'Find out more information by clicking on the "Help" tab above.', 'startbox' ) . '</p>';
 		echo '<p>' . sprintf( __( 'You can also visit the StartBox <a href="%s" target="_blank">support forum</a>', 'startbox' ), 'http://wpstartbox.com/support/' ) . '</p>';
+		echo '<p>' . sprintf( __( '<a href="%s" target="_blank">StartBox Website</a> | <a href="%s" target="_blank">Documentation</a> | <a href="%s" target="_blank">Github</a> | <a href="%s" target="_blank">Twitter</a> | <a href="%s" target="_blank">Facebook</a>', 'startbox' ), 'http://wpstartbox.com', 'http://docs.wpstartbox.com/Main_Page', 'https://github.com/WebDevStudios/StartBox/', 'http://twitter.com/wpstartbox', 'https://www.facebook.com/pages/WP-StartBox/465761230178685' ) .'</p>';
 	}
 
 }
 
-sb_register_settings('sb_settings_help');
+sb_register_settings( 'sb_settings_help' );
