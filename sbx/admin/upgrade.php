04ba9cb2425ed3f0f2a1cd179ec0533dd563c902
Brad Williams <brad@webdevstudios.com>
2013-10-30 09:22:29 -0400

    Remove option to disable auto-updates

--- sbx/admin/upgrade.php
+++ sbx/admin/upgrade.php
@@ -5,7 +5,7 @@
 class sb_upgrade_settings extends sb_settings {
 
 	function sb_upgrade_settings() {
-		$this->name = __( 'Version Information', 'startbox');
+		$this->name = __( 'SBX Information', 'startbox' );
 		$this->slug = 'sb_upgrade_settings';
 		$this->location = 'primary';
 		$this->priority = 'high';
@@ -13,18 +13,15 @@ class sb_upgrade_settings extends sb_settings {
 			'sb_version_info' => array(
 				'type'	=> 'intro',
 				'desc'	=> sprintf( __( 'StartBox Version: %s', 'startbox' ), SB_VERSION )
-			),
-			'enable_updates' => array(
-					'type'		=> 'checkbox',
-					'label'		=> __( 'Enable Automatic Updates', 'startbox' ),
-					'default'	=> 'true'
-			),
+			)
 		);
+
 		parent::__construct();
+
 	}
 
 }
 
 // Only register this panel if the theme supports upgrades
-if ( current_theme_supports('sb-updates') )
+if ( current_theme_supports( 'sb-updates' ) )
 	sb_register_settings( 'sb_upgrade_settings' );
