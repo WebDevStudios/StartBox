6952dc3c3002583984365fe49a5fe40d3f7e717e
Greg Rickaby <gregrickaby@gmail.com>
2013-12-20 10:52:56 -0600

    Changed comment text from “Thoughts” to “Comments”

--- comments.php
+++ comments.php
@@ -23,7 +23,7 @@ if ( post_password_required() )
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'startbox' ),
+				printf( _nx( 'One comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'startbox' ),
 					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
 			?>
 		</h2>
