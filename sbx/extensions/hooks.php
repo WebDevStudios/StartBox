644942e4e29c9ca2539a2be5125417bffa0a83f7
Brian Richards <brian@rzen.net>
2013-12-20 00:26:02 -0500

    Update default actions/filters - Release Ready!

--- sbx/extensions/hooks.php
+++ sbx/extensions/hooks.php
@@ -1,260 +1,12 @@
 <?php
 /**
- * StartBox Hooks and Filters
+ * SBX Common Actions and Filters.
  *
- * A collection of many of StartBox's default hooks, as well as some default filtration.
- *
- * @package StartBox
- * @subpackage Functions
+ * @package SBX
+ * @subpackage Extensions
+ * @since 1.0.0
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
-
-////////////////////////////////////////////////// Body, Post and Comment class filters //////////////////////////////////////////////////
-
-// Filter body_class to include user browser, category, and date classes
-function sbx_body_classes($classes) {
-	global $wp_query;
-
-	// Determine user's browser and adds appropriate class
-	global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
-	if($is_lynx) $classes[]       = 'lynx';
-	elseif($is_gecko) $classes[]  = 'gecko';
-	elseif($is_opera) $classes[]  = 'opera';
-	elseif($is_NS4) $classes[]    = 'ns4';
-	elseif($is_safari) $classes[] = 'safari';
-	elseif($is_chrome) $classes[] = 'chrome';
-	elseif($is_IE) $classes[]     = 'ie';
-	elseif($is_iphone) $classes[] = 'iphone';
-	else $classes[]               = 'unknown';
-
-	// Include user's IE version for version-specific hacking. Credit: http://wordpress.org/extend/plugins/krusty-msie-body-classes/
-	if( preg_match( '/MSIE ([0-9]+)([a-zA-Z0-9.]+)/', $_SERVER['HTTP_USER_AGENT'], $browser_version ) ){
-
-		// add a class with the major version number
-		$classes[] = 'ie' . $browser_version[1];
-
-		// add an ie-lt9 class to match MSIE 8 and older
-		if ( 9 > $browser_version[1] )
-			$classes[] = 'ie-lt9';
-
-		// add an ie-lt8 and ie-old class to match MSIE 7 and older
-		if ( 8 > $browser_version[1] ) {
-			$classes[] = 'ie-lt8';
-			$classes[] = 'ie-old';
-		}
-
-	}
-
-	// Adds category classes for each category on single posts
-	if ( $cats = get_the_category() )
-		foreach ( $cats as $cat )
-			$classes[] = 's-category-' . $cat->slug;
-
-	// Applies the time- and date-based classes
-    sbx_date_classes( time(), $classes, $p = null );
-
-	// Adds classes for the month, day, and hour when the post was published
-	if ( is_single() )
-		sbx_date_classes( mysql2date( 'U', $wp_query->post->post_date ), $classes, 's-' );
-
-	// Adds post and page slug class, prefixed by 'post-' or 'page-', respectively
-	if ( is_single() )
-    	$classes[] = 'post-' . $wp_query->post->post_name;
-	elseif( is_page() )
-		$classes[] = 'page-' . $wp_query->post->post_name;
-
-	// Gutters
-	$classes[] = 'gutters';
-
-	// return the $classes array
-	return $classes;
-}
-add_filter('body_class','sbx_body_classes');
-
-
-// Filter post_class to include an author class
-function sbx_post_classes($classes) {
-	// Author for the post queried
-	$classes[] = 'author-' . sanitize_title_with_dashes( strtolower( get_the_author() ) );
-
-	// return the $classes array
-	return $classes;
-}
-add_filter('post_class','sbx_post_classes');
-
-
-// Generates time- and date-based classes relative to GMT (UTC)
-function sbx_date_classes($t, &$classes, $p) {
-	$t = $t + ( get_option('gmt_offset') * 3600 );
-	$classes[] = $p . 'y' . gmdate( 'Y', $t ); // Year
-	$classes[] = $p . 'm' . gmdate( 'm', $t ); // Month
-	$classes[] = $p . 'd' . gmdate( 'd', $t ); // Day
-	$classes[] = $p . 'h' . gmdate( 'H', $t ); // Hour
-}
-
-
-////////////////////////////////////////////////// Items To Hook into Header //////////////////////////////////////////////////
-
-// Header Wrap
-function sbx_header_wrap() {
-	if ( !did_action( 'sb_header') )
-		echo '<div id="header_wrap">'."\n";
-	else
-		echo '</div><!-- #header_wrap -->'."\n";
-}
-add_action( 'before_header', 'sbx_header_wrap', 999 );
-add_action( 'after_header', 'sbx_header_wrap', 9 );
-
-/**
- * Filter the site title to be more dynamic.
- *
- */
-function sbx_default_title( $title, $sep, $seplocation) {
-	$site_name = get_bloginfo('name');
-	$sep = ' | ';
-
-	if ( is_home() || is_front_page() ) { $title = get_bloginfo('description'); }
-	elseif ( is_singular() || is_page() ) { $title = single_post_title( '', false ); }
-	elseif ( is_search() ) { $title = sprintf( __('Search Results for: %s', 'startbox'), esc_html(stripslashes(get_search_query())) ); }
-    elseif ( is_category() ) { $title = sprintf( __('Category Archives: %s', 'startbox'), single_cat_title( '', false )); }
-    elseif ( is_tag() ) { $title = sprintf( __('Tag Archives: %s', 'startbox'), sbx_tag_query() ); }
-	elseif ( is_404() ) { $title = __( 'Not Found', 'startbox' ); }
-	else { $title = get_bloginfo('description'); }
-
-	// Appends current page number (if on page 2 or greater)
-    if ( get_query_var('paged') ) { $title .= $sep . sprintf( __( 'Page %s', 'startbox' ), get_query_var('paged') ); }
-
-	if ( is_home() || is_front_page() ) {
-		$title = array(
-			'site_name' => $site_name,
-			'separator' => $sep,
-			'title'     => $title
-		);
-	} else {
-		$title = array(
-			'title'     => $title,
-			'separator' => $sep,
-			'site_name' => $site_name
-		);
-	}
-
-    // Filters should return an array
-    $title = apply_filters('sb_doctitle', $title);
-
-	if( is_array( $title ) )
-		$title = implode('', $title);
-
-	return $title;
-}
-add_filter( 'wp_title', 'sbx_default_title', 9, 3 );
-
-// Filter the RSS title to return nothing, otherwise RSS shows dupilicate title
-add_filter( 'wp_title_rss', create_function( '$a', 'return "";' ) );
-
-// Insert #top anchor at beginning of page
-function sbx_topofpage() {
-	echo '<a name="top"></a>'."\n";
-}
-add_action( 'before', 'sbx_topofpage', 1);
-
-// Insert skip-to-content link for screen reader users
-function sb_skip_to_content() {
-	echo '<a href="#content" title="Skip to content" class="skip-to-content">' . __( 'Skip to content', 'startbox' ) . '</a>'."\n";
-}
-
-// Insert Yoast Breadcrumbs if Active
-function sbx_breadcrumb_output() {
-	if ( function_exists( 'yoast_breadcrumb' ) ) { yoast_breadcrumb('<div id="yoastbreadcrumb">','</div>'); }
-}
-add_action( 'before_content', 'sbx_breadcrumb_output', 15 );
-
-
-////////////////////////////////////////////////// Items To Hook into home page //////////////////////////////////////////////////
-
-// Add additional hooks to the front page
-function sbx_front_page_hooks() {
-	// Only include these hooks on the front page,
-	// and when NOT using a custom template
-	if ( is_front_page() && ! is_page_template() ) {
-		do_action( 'before_featured' );
-		do_action( 'featured' );
-		do_action( 'after_featured' );
-	}
-}
-add_action( 'before_content', 'sbx_front_page_hooks' );
-
-// Add a featured widget area to sb_featrued on the home page
-function sbx_home_featured_sidebar() {
-	sbx_do_sidebar( 'featured_aside', 'home_featured', 'featured-aside' );
-}
-add_action( 'featured','sbx_home_featured_sidebar');
-
-////////////////////////////////////////////////// Items To Hook into content areas //////////////////////////////////////////////////
-
-// Filter the page title based on the template (credit: Ian Stewart)
-function sbx_default_page_title() {
-	global $post;
-	$container = apply_filters( 'sb_page_title_container', 'h1' );
-
-	$content = '<' . $container . ' class="page-title">';
-	if (is_attachment()) {
-		$content .= '<a href="';
-		$content .= get_permalink($post->post_parent);
-		$content .= '" rev="attachment"><span class="meta-nav">&laquo; </span>';
-		$content .= get_the_title($post->post_parent);
-		$content .= '</a>';
-	} elseif ( is_singular() ) {
-		$content .= get_the_title();
-	} elseif (is_author()) {
-		$content .= '<span>';
-		$content .= __('Author Archives: ', 'startbox');
-		$content .= '</span> ';
-		$content .= get_the_author();
-	} elseif (is_category()) {
-		$content .= '<span>';
-		$content .= __('Category Archives:', 'startbox');
-		$content .= '</span> ';
-		$content .= single_cat_title('', FALSE);
-	} elseif (is_search()) {
-		$content .= '<span>';
-		$content .= __('Search Results for:', 'startbox');
-		$content .= '</span> <span id="search-terms">';
-		$content .= esc_html(stripslashes($_GET['s']), true);
-		$content .= '</span>';
-	} elseif (is_tag()) {
-		$content .= '<span>' . __('Tag Archives:', 'startbox') . '</span> ' . sbx_tag_query();
-	} elseif (is_tax()) {
-		$term = get_term_by('slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
-		$content .= '<span>' . __('Archives:', 'startbox') . '</span> ' . $term->name;
-	} elseif (is_day()) {
-		$content .= sprintf(__('<span>Daily Archives:</span> %s', 'startbox'), get_the_time(get_option('date_format')));
-	} elseif (is_month()) {
-		$content .= sprintf(__('<span>Monthly Archives:</span> %s', 'startbox'), get_the_time('F Y'));
-	} elseif (is_year()) {
-		$content .= sprintf(__('<span>Yearly Archives:</span> %s', 'startbox'), get_the_time('Y'));
-	} elseif (is_404()) {
-		$content .= __('404 - File Not Found', 'startbox');
-	} elseif (is_post_type_archive()) {
-		$content .= '<span>' . __('Content Archives:', 'startbox') . '</span> ' . post_type_archive_title( '', false );
-	} elseif (isset($_GET['paged']) && !empty($_GET['paged'])) {
-		$content .= __('Blog Archives', 'startbox');
-	}
-	$content .= '</' . $container . '>';
-	$content .= "\n";
-
-	echo apply_filters( 'sbx_default_page_title', $content, $container, $post );
-}
-add_action( 'page_title', 'sbx_default_page_title' );
-
-// Hook archive meta after page title for archive pages
-function sbx_archive_meta() {
-	if ( ( is_category() || is_tag() || is_tax() ) && term_description() != '' ) {
-		$content = '<div class="archive-meta">';
-		$content .= apply_filters( 'sbx_archive_meta', term_description() );
-		$content .= '</div>';
-		echo $content;
-	}
-}
-add_action( 'page_title', 'sbx_archive_meta' );
 
 // Add content filters for the description/meta content
 add_filter( 'archive_meta', 'wptexturize' );
@@ -262,34 +14,250 @@ add_filter( 'archive_meta', 'convert_smilies' );
 add_filter( 'archive_meta', 'convert_chars' );
 add_filter( 'archive_meta', 'wpautop' );
 
-// Default 404 Page
-function sbx_404_content() {
-	echo '<p>' . __('Sorry, but we were unable to find what you were looking for. Try searching our content below.', 'startbox' ) . '</p>';
-	get_template_part( 'searchform' );
-}
-add_action( '404', 'sbx_404_content' );
+// Enable Shortcodes in widget areas
+add_filter( 'widget_text', 'do_shortcode' );
 
-// Dynamically create hook for the very first post in a loop
-function sbx_before_first_post() { global $firstpost; if ( !isset( $firstpost ) ) { do_action( 'before_first_post' ); } } // Just before the post
-function sbx_after_first_post() { global $firstpost; if ( !isset( $firstpost ) ) { do_action( 'after_first_post' ); $firstpost = 1; } } // Just after the post
+/**
+ * Add helpful CSS classes to the <body> tag
+ * on singular content views.
+ *
+ * @since  1.0.0
+ *
+ * @param  array $classes Body classes.
+ * @return array          Modified body classes.
+ */
+function sbx_body_classes( $classes = array() ) {
+	global $wp_query;
+
+	// Bail here if not viewing a singular object
+	if ( ! is_singular() )
+		return $classes;
+
+	// Add classes for the month, day, and hour of publication
+	$classes = sbx_date_classes( mysql2date( 'U', $wp_query->post->post_date ), $classes, 's-' );
+
+	// Add class for the content's slug
+	$classes[] = 'slug-' . $wp_query->post->post_name;
+
+	// For posts, add a class for each category
+	if ( is_single() && $cats = get_the_category() ) {
+		foreach ( $cats as $cat ) {
+			$classes[] = 's-category-' . $cat->slug;
+		}
+	}
+
+	// return the $classes array
+	return $classes;
+
+}
+add_filter( 'body_class','sbx_body_classes' );
+
+/**
+ * Add helpful classes to the post container.
+ *
+ * @since  1.0.0
+ *
+ * @param  array  $classes Post classes.
+ * @return array           Modified post classes.
+ */
+function sbx_post_classes( $classes = array() ) {
+
+	// Add classes for the month, day, and hour of publication
+	$classes = sbx_date_classes( get_the_time( 'U' ), $classes, 's-' );
+
+	// Add class for the post author
+	$classes[] = 'author-' . sanitize_title_with_dashes( strtolower( get_the_author() ) );
+
+	return $classes;
+}
+add_filter( 'post_class', 'sbx_post_classes' );
+
+/**
+ * Filter wp_title to provide a more well-rounded site title.
+ *
+ * @since  1.0.0
+ *
+ * @param  string $title Default title for current view.
+ * @param  string $sep   Optional separator.
+ * @return string        Filtered title.
+ */
+function sbx_default_site_title( $title, $sep ) {
+	global $page, $paged;
+
+	// Get the site name
+	$site_name = get_bloginfo('name');
+
+	// Return only the content title for feeds
+	if ( is_feed() ) {
+		return $title;
+	}
+
+	// Get the most relevant title based on content being viewed
+	if ( is_singular() ) {
+		$title = single_post_title( '', false );
+	} elseif ( is_category() ) {
+		$title = sprintf( __( 'Category Archives: %s', 'sbx' ), single_term_title( '', false ) );
+	} elseif ( is_tag() ) {
+		$title = sprintf( __( 'Tag Archives: %s', 'sbx' ), single_term_title( '', false ) );
+	} elseif ( is_tax() ) {
+		$taxonomy = get_query_var( 'taxonomy' );
+		$title = sprintf( __( '%1$s Archives: %2$s', 'sbx' ), $taxonomy->singular_label, single_term_title( '', false ) );
+	} elseif ( is_author() ) {
+		the_post();
+		$author = get_the_author();
+		rewind_posts();
+		$title = sprintf( __( 'Author Archives: %s', 'sbx' ), esc_html( $author ) );
+	} elseif ( is_search() ) {
+		$title = sprintf( __( 'Search Results for: %s', 'sbx' ), esc_html( stripslashes( get_search_query() ) ) );
+	} elseif ( is_404() ) {
+		$title = __( 'Not Found', 'sbx' );
+	} else {
+		$title = get_bloginfo( 'description' );
+	}
+
+	// Add a page number if necessary:
+	if ( $paged >= 2 || $page >= 2 ) {
+		$title .= " $sep " . sprintf( __( 'Page %s', 'sbx' ), max( $paged, $page ) );
+	}
+
+	// Handle the home/blog pages differently than everything else
+	if ( is_home() || is_front_page() ) {
+		$new_title = array(
+			'site_name' => $site_name,
+			'separator' => $sep,
+			'title'     => $title
+		);
+	} else {
+		$new_title = array(
+			'title'     => $title,
+			'separator' => $sep,
+			'site_name' => $site_name
+		);
+	}
+
+	return implode( ' ', $new_title );
+
+}
+add_filter( 'wp_title', 'sbx_default_site_title', 9, 2 );
+
+/**
+ * Add a "Page Template" column to the 'Page' dashboard.
+ *
+ * This column shows the page template in use by the given page.
+ *
+ * Credit: @tommcfarlin
+ * @link http://tommcfarlin.com/view-page-templates/
+ *
+ * @since  1.0.0
+ * @param  array $page_columns Table columns.
+ * @return array               Modified table columns.
+ */
+function sbx_add_template_column( $page_columns ) {
+	$page_columns['template'] = __( 'Page Template', 'sbx' );
+	return $page_columns;
+}
+add_filter( 'manage_edit-page_columns', 'sbx_add_template_column' );
+
+/**
+ * Output friendly name of template used by page.
+ *
+ * Sanely falls back to "Default" if no template is in use.
+ *
+ * Credit: @tommcfarlin
+ * @link http://tommcfarlin.com/view-page-templates/
+ *
+ * @since 1.0.0
+ * @param array $page_columns Column being rendered.
+ */
+function sbx_add_template_data( $column_name ) {
+	global $post;
+
+	// Only output data for the custom "template" column
+	if ( 'template' !== $column_name ) {
+		return;
+	}
+
+	// First, the get name of the template
+	$template_name = get_post_meta( $post->ID, '_wp_page_template', true );
+	$parent_theme_template = get_template_directory() . '/' . $template_name;
+	$child_theme_tempalte = get_stylesheet_directory() . '/' . $template_name;
+
+	// Output "Default" if template not set or non-existant
+	if ( 0 == strlen( trim( $template_name ) ) || ( ! file_exists( $parent_theme_template ) || ! file_exists( $child_theme_template ) ) ) {
+		_e( 'Default', 'sbx' );
+
+	// Otherwise, output the friendly template name
+	} elseif ( file_exists( $parent_theme_template ) ) {
+		echo get_file_description( $parent_theme_template );
+	} elseif ( file_exists( $child_theme_template ) ) {
+		echo get_file_description( $child_theme_template );
+	}
+
+}
+add_action( 'manage_page_posts_custom_column', 'sbx_add_template_data' );
+
+/**
+ * Auto-hide the address bar in mobile Safari (iPhone)
+ *
+ * @since  1.0.0
+ */
+function sbx_hide_iphone_addressbar() {
+	echo '<script type="text/javascript">window.scrollTo(0, 1);</script>';
+}
+add_action( 'wp_footer','sbx_hide_iphone_addressbar' );
+
+/**
+ * Dynamically add a "before_first_post" hook to first post in a loop.
+ *
+ * @since 1.0.0
+ */
+function sbx_before_first_post() {
+	if ( ! did_action('before_first_post') ) {
+		do_action( 'before_first_post' );
+	}
+}
 add_action( 'before_post', 'sbx_before_first_post' );
+
+/**
+ * Dynamically add a "before_first_post" hook to first post in a loop.
+ *
+ * @since 1.0.0
+ */
+function sbx_after_first_post() {
+	if ( ! did_action('after_first_post') ) {
+		do_action( 'after_first_post' );
+	}
+}
 add_action( 'after_post', 'sbx_after_first_post' );
 
-////////////////////////////////////////////////// Items To Hook into Footer //////////////////////////////////////////////////
+/**
+ * Forever eliminate "Startbox" from the planet
+ * (or at least the little bit we can influence).
+ *
+ * Violating our coding standards for a good function name.
+ * Adapted from capital_P_dangit().
+ *
+ * @since 1.0.0
+ */
+function capital_B_dangit( $text ) {
 
-// Include our footer widgets
-function sbx_footer_widgets() {
-	get_sidebar('footer');
+	// Simple replacement for titles
+	if ( 'the_title' === current_filter() )
+		return str_replace( 'Startbox', 'StartBox', $text );
+
+	// Still here? Use the more judicious replacement
+	static $dblq = false;
+	if ( false === $dblq ) {
+		$dblq = _x( '&#8220;', 'opening curly quote' );
+	}
+
+	return str_replace(
+		array( ' Startbox', '&#8216;Startbox', $dblq . 'Startbox', '>Startbox', '(Startbox' ),
+		array( ' StartBox', '&#8216;StartBox', $dblq . 'StartBox', '>StartBox', '(StartBox' ),
+		$text
+	);
+
 }
-add_action( 'footer_widgets', 'sbx_footer_widgets' );
-
-// Auto-hide the address bar in mobile Safari (iPhone)
-function sbx_iphone() { echo '<script type="text/javascript">window.scrollTo(0, 1);</script>'; }
-add_action( 'after','sbx_iphone');
-
-// Add left/right footer hooks
-function sbx_footer_left_right() {
-	if ( has_action( 'sb_footer_left' ) ) { echo '<div id="footer_left" class="left">'; do_action( 'footer_left' ); echo '</div><!-- #footer_left -->'; }
-	if ( has_action( 'sb_footer_right' ) ) { echo '<div id="footer_right" class="right">'; do_action( 'footer_right' ); echo '</div><!-- #footer_right -->'; }
-}
-add_action( 'footer', 'sbx_footer_left_right', 15 );
+add_filter( 'the_content', 'capital_B_dangit', 11 );
+add_filter( 'the_title', 'capital_B_dangit', 11 );
+add_filter( 'comment_text', 'capital_B_dangit', 31 );
